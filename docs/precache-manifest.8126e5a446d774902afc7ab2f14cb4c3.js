8c7356223d2335c67a4671bd5991221c184b8474
Yandiel Lillo <lillo.yandiel@seiza-ti.cl>
2020-09-21 10:06:25 -0300

    Agregando docs

---
+++ docs/precache-manifest.8126e5a446d774902afc7ab2f14cb4c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132a5e722eac9b08ec3ad6224229a258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "337c3c82f4b4d92c9ce8",
+    "url": "/static/css/main.6bfe1c37.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337c3c82f4b4d92c9ce8",
+    "url": "/static/js/main.789df085.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
